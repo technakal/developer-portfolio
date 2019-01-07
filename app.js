09308d04973a059cff44040bda58cdf7e2be2a34
Noel Keener <noel@nakeener.com>
2019-01-07 06:09:51 -0600

    Add server console logging of errors

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ app.use('/', (req, res, next) => {
 app.use((err, req, res, next) => {
   res.locals.error = err;
   res.status(err.status);
+  console.log(`${err.message} - ${err.status}`);
+  console.log(err.stack);
   res.render('error');
 });
 
